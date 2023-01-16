cf62e07e76c08bb71b58915a3674e32b41a9b254
xammyboii <118329638+xammyboii@users.noreply.github.com>
2023-01-16 10:57:42 -0600

    add event-detail component/folder

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,17 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
     <div>
       <h1>Event Detail</h1>
