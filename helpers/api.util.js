27adaeb19ddbc354395ac117ba5c873146e3992f
xammyboii <xavieryabados.09@gmail.com>
2023-02-13 13:08:07 -0600

    added getEventById function

--- helpers/api.util.js
+++ helpers/api.util.js
@@ -3,7 +3,6 @@ export async function getAllEvents() {
     "https://nextjs-course-47eba-default-rtdb.firebaseio.com/events.json"
   );
   const data = await response.json();
-
   const events = [];
 
   for (const key in data) {
@@ -20,3 +19,8 @@ export async function getFeaturedEvents() {
   const allEvents = await getAllEvents();
   return allEvents.filter((event) => event.isFeatured);
 }
+
+export async function getEventById(id) {
+    const allEvents = await getAllEvents();
+    return allEvents.find((event) => event.id === id);
+  }
